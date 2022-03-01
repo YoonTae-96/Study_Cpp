0e923f631826d537903d3ae3a1d4407af98515f2
YoonTae-96 <82247234+YoonTae-96@users.noreply.github.com>
2022-03-01 15:54:43 +0900

    Create week1_while.cpp

---
+++ Chapter1/week1_while.cpp
@@ -0,0 +1,13 @@
+/*#include <iostream>
+
+int main() {
+	int sum = 0;
+	int va1 = 1;
+	while (va1 <= 10) {
+		sum += va1;
+		++va1;
+	}
+
+	std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+	return 0;
+}*/
