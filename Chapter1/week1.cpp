a1d0b7be0742ab7e1e7890482506cd94950abe39
YoonTae-96 <82247234+YoonTae-96@users.noreply.github.com>
2022-03-01 15:54:39 +0900

    Create week1.cpp

---
+++ Chapter1/week1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/*int main() {
+	std::cout << "Enter two numbers:" << std::endl; //std::endl -> 버퍼를 비워주는 역할
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2; // :: 연산자는, 범위 지정 연산자다.
+	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+
+	return 0;
+}*/
